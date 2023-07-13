6bcd115ca6e6edcbd115e7cf6fc170dfd584aae4
Fernando-Ruiz176 <ruizfe.sud@gmail.com>
2023-07-12 23:33:27 -0400

    Initial commit

---
+++ app/src/main/java/cl/fernando/individual10m5/FoldablePhone.kt
@@ -0,0 +1,23 @@
+package cl.fernando.individual10m5
+
+class FoldablePhone (isScreenLightOn: Boolean = false) : Phone(isScreenLightOn) {
+    var isFoldable : Boolean = false
+    override fun  switchOn(){
+        if(!isFoldable){
+            isScreenLightOn = true
+        }
+    }
+    fun abrir (){
+        isFoldable = true
+    }
+    fun cerrar (){
+        isFoldable = false
+    }
+}
+
+fun main(){
+    val foldable = FoldablePhone()
+    println(foldable.isFoldable)
+    foldable.abrir()
+    println(foldable.isFoldable)
+}
